aed0c749a7d1865caf1f75714205bef00f81b48c
Najmul Alam Miraj <najmulalammiraj@gmail.com>
2022-05-26 02:33:59 +0600

    m

---
+++ js/music-list.js
@@ -0,0 +1,128 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Mone Lage Bhoy Go Allah",
+    artist: "Sheikh Afzal Hussain Sadi",
+    img: "music-000",
+    src: "music-000"
+  },
+  {
+    name: "Tomar Premer Sur",
+    artist: "Mamunur Rashid",
+    img: "music-00",
+    src: "music-00"
+  },
+  {
+    name: "Mera Pyara Nabi",
+    artist: "Mamunur Rashid",
+    img: "music-0",
+    src: "music-0"
+  },
+  {
+    name: "Muhammad Rasul",
+    artist: "Sadikur Rahman , Mushir Alom Mijan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Paina Khuje Shur",
+    artist: "Sheikh Afzal Hussain Sadi",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Rasul Namer",
+    artist: "Sheikh Afzal Hussain Sadi",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Marhaba Ya Mustafa",
+    artist: "Mushir Alom Mijan , Sayeem Ahmod Emon",
+    img: "music-4",
+    src: "music-4"
+  },
+  // {
+  //   name: "Meherban",
+  //   artist: "Zakaria Ahmad Shahan",
+  //   img: "music-5",
+  //   src: "music-5"
+  // },
+  // {
+  //   name: "Allahu Allah",
+  //   artist: "Anwar Hussain Gulzar",
+  //   img: "music-6",
+  //   src: "music-6"
+  // },
+  {
+    name: "Satto Pather Disa",
+    artist: "Sheikh Afzal Hussain Sadi",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Mustofa Mujtoba",
+    artist: "Sheikh Afzal Hussain Sadi",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Ami Nisite",
+    artist: "Sheikh Afzal Hussain Sadi",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Tomar Dedar Cheya",
+    artist: "Sheikh Afzal Hussain Sadi",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Allahu Allahu ",
+    artist: "Mushir Alom Mijan , Sayeem Ahmod Emon",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Madiney Ki Gali ",
+    artist: "Mushir Alom Mijan , Sayeem Ahmod Emon",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Shorger Shuk Chai Na ",
+    artist: "Sheikh Afzal Hussain Sadi",
+    img: "music-12",
+    src: "music-13"
+  },
+  {
+    name: "Gai Tomari Shan ",
+    artist: "Abdullah Al Farhan",
+    img: "music-12",
+    src: "music-14"
+  },
+  {
+    name: "Rasul Namer ",
+    artist: "Abdullah Al Farhan",
+    img: "music-12",
+    src: "music-15"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
